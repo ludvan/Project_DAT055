8db386388ffba55d9b96301d6e9a7072f4704feb
ludvan <ludvan@student.chalmers.se>
2022-02-11 17:13:20 +0100

    ColorChoice bör funka
    Färg väljs och skickas till server (förhoppningsvis)

--- ColorChoice.java
+++ ColorChoice.java
@@ -5,16 +5,21 @@ import javax.swing.*;
 
 public class ColorChoice extends JPanel implements ActionListener {
 	
+	private ChatClient Choiceclient;
 	
 	private JButton redB;
 	private JButton blueB;
 	private JButton greenB;
 	private JButton yellowB;
 	
+	private Col ChosenColor;
+	
 	private boolean visible;
 	
-	ColorChoice(){
+	ColorChoice(ChatClient currentClient){
 		setLayout(new FlowLayout());
+		
+		Choiceclient = currentClient;
 		
 		redB = new JButton("red");
 		blueB = new JButton("blue");
@@ -33,8 +38,21 @@ public class ColorChoice extends JPanel implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		//logik för att byta färg insert
-		
+		switch(e.getActionCommand()) {
+		case "red":
+			ChosenColor = Col.red;
+		case "blue":
+			ChosenColor = Col.blue;
+		case "green":
+			ChosenColor = Col.green;
+		case "yellow":
+			ChosenColor = Col.yellow;
+		default:
+			System.out.println("didn't get a valid color");
+		}
+		//inte snyggt
+		TransmitData data = new TransmitData(new Card(Value.zero,Col.blue), -100, ChosenColor, false, true);
+		Choiceclient.sendToServer(data);
 		
 		toggle();
 		
