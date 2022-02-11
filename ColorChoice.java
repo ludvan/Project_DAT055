8ad1047d8342765e40df46589137e59ac916f16b
ludvan <ludvan@student.chalmers.se>
2022-02-11 20:24:35 +0100

    "Går inte" att kryssa ner ColorChoice
    Egentligen öppnas bara en ny ruta om man försöker.
    setDefaultCloseOperation skulle funka, men då måste fönstret skapas på annat sätt, knapparna funkade ej när jag försökte med det

--- ColorChoice.java
+++ ColorChoice.java
@@ -1,6 +1,3 @@
-import java.awt.*;
-import java.awt.event.*;
-
 import javax.swing.*;
 
 public class ColorChoice{
@@ -16,30 +13,34 @@ public class ColorChoice{
 
 	public void ChooseColor(){
 		int answer = JOptionPane.showOptionDialog(null, "Choose a color", "",JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-
-		switch(answer) {
-		case 0:
-			ChosenColor = Col.red;
-			break;
-		case 1:
-			ChosenColor = Col.blue;
-			break;
-		case 2:
-			ChosenColor = Col.yellow;
-			break;
-		case 3:
-			ChosenColor = Col.green;
-			break;
-		default:
-			throw new IllegalArgumentException("Unknown input");
+		
+		//answer = -1 om man stänger fönstret istället för att välja något
+		if(answer == -1) {
+			ChooseColor();
+		}else {
+			switch(answer) {
+			case 0:
+				ChosenColor = Col.red;
+				break;
+			case 1:
+				ChosenColor = Col.blue;
+				break;
+			case 2:
+				ChosenColor = Col.yellow;
+				break;
+			case 3:
+				ChosenColor = Col.green;
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown input");
+				
+			}
 			
+			//bör ses över
+			TransmitData data = new TransmitData(new Card(Value.zero,Col.blue), -100, ChosenColor, false, true);
+			ChoiceClient.sendToServer(data);
 		}
 		
-		//bör ses över
-		TransmitData data = new TransmitData(new Card(Value.zero,Col.blue), -100, ChosenColor, false, true);
-		ChoiceClient.sendToServer(data);
 	}
-	
-	
 
 }
