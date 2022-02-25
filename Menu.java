b3e9251a9001118b753b738013be5a65b7e88358
dagbrynildsentholander <81674202+dagbrynildsentholander@users.noreply.github.com>
2022-02-25 13:05:49 +0100

    fixes

--- Menu.java
+++ Menu.java
@@ -29,16 +29,12 @@ public class Menu extends JFrame{
         try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (InstantiationException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (IllegalAccessException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (UnsupportedLookAndFeelException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
         clientProcess = new ArrayList<Process>();
